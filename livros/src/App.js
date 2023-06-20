8eda416f37cd2ee0443f3fa49e0b17e03ab99311
andresouzab <andre.desouzabb@gmail.com>
2023-06-19 21:47:24 -0300

    primeira_aula

---
+++ livros/src/App.js
@@ -0,0 +1,21 @@
+// Importações da Aplicação:
+import logo from './logo.svg';
+import './App.css';
+import MenuSuperior from './components/MenuSuperior';
+
+const App = () => {
+  return ( // Tudo que vai aqui dentro aparece na aplicação.
+  <>
+  <MenuSuperior/>
+  
+  </>  
+  
+);
+}
+
+// próximos passos:
+// MenuSuperior.js
+// InclusaoLivros.js
+
+
+export default App;
